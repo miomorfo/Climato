4d9ad1d82dbc54716589a2baa10dbeeb7d86c0ed
Fernando <fernando.eb@icloud.com>
2022-01-24 18:05:46 -0300

    Initial Commit

---
+++ Climato/ClimatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimatoApp.swift
+//  Climato
+//
+//  Created by Fernando on 24-01-22.
+//
+
+import SwiftUI
+
+@main
+struct ClimatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
